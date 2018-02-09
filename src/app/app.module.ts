ab93c1eed366e38c79e7bbc0bf7841a1058878fc
Saidul Hofmann <saidul.hofmann@gmail.com>
2018-02-09 17:33:22 +0100

    - Remove ng-bootstrap wrapper. - Add bootstrap default configuration. - Add responsive page layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
@@ -13,7 +12,6 @@ import { AppNavbarComponent } from './app-navbar/app-navbar.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
