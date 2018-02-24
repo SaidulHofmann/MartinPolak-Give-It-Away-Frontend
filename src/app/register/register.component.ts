c24481a8a7f132afb0af1d50b64591cae4b71d83
Martin Polak <martin.polak@hsr.ch>
2018-02-24 19:51:51 +0100

    user registration implemented (first approach) - registration component - user model - user service

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,8 @@
 /* Creates an user account */
 
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {UserService} from '../_services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -9,8 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  model: any = {};
+  loading = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private userService: UserService) { }
+
+  register() {
+    this.loading = true;
+    this.userService.register(this.model)
+      .subscribe(
+        data => {
+          // set success message and pass true paramater to persist the message after redirecting to the login page
+
+          this.router.navigate(['/login']);
+        },
+        error => {
+
+          this.loading = false;
+        });
+  }
 
   ngOnInit() {
   }
