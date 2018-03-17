527546c0c0ad482920f6ac0113d6c4560bb7cbbe
Saidul Hofmann <saidul.hofmann@gmail.com>
2018-03-17 23:47:30 +0100

    Article form: Define validation and set the correct category and status.

--- src/app/containers/article-new/article-new.component.ts
+++ src/app/containers/article-new/article-new.component.ts
@@ -56,106 +56,132 @@ export class ArticleNewComponent implements OnInit, OnChanges {
     );
   }
 
+  /**
+   * Creates the article form for creating or updating articles.
+   */
   createForm() {
     console.log('@ArticleNewComponent.createForm()');
     // Article
     this.articleForm =    this.formBuilder.group({
-      category:           this.article.category || articleCategories[0],
+      _id:                this.article._id || null,
+      category:           this.getArticleCategory(this.article.category._id),
       name:               [this.article.name || '', Validators.required ],
       description:        [this.article.description || '', Validators.required ],
       handover:           [this.article.handover || '', Validators.required ],
+
       pictureOverview:    this.article.pictureOverview || '',
       pictures:           this.formBuilder.array( []),
       videos:             this.formBuilder.array( []),
       tags:               this.article.tags || '',
-      status:             this.article.status || articleStatus[0],
+      status:             this.getArticleStatus(this.article.status._id),
 
       // Give away
-      donationDate:       this.article.donationDate || null,
       donee:              this.formBuilder.group({
         _id:              this.article.donee ? this.article.donee._id : null,
         firstname:        this.article.donee ? this.article.donee.firstname : '',
         lastname:         this.article.donee ? this.article.donee.lastname : ''
       }),
+      donationDate:       this.article.donationDate || null,
 
-      // Publisher
+      // Publishing
       publisher:          this.formBuilder.group({
-        _id:              this.article.publisher ? this.article.publisher._id : null,
+        _id:              [this.article.publisher ? this.article.publisher._id : null, Validators.required ],
         firstname:        this.article.publisher ? this.article.publisher.firstname : '',
         lastname:         this.article.publisher ? this.article.publisher.lastname : ''
       }),
       createdAt:          this.article.createdAt || null,
-      updatedAt:          this.article.updatedAt || null,
-      _id:                this.article._id || null
-
+      updatedAt:          this.article.updatedAt || null
     });
   }
 
   // FormControl Getters
   get name() { return this.articleForm.get('name'); }
+  get description() { return this.articleForm.get('description'); }
+  get handover() { return this.articleForm.get('handover'); }
   get pictureOverview() { return this.articleForm.get('pictureOverview'); }
   get pictures(): FormArray { return this.articleForm.get('pictures') as FormArray; }
   get videos(): FormArray { return this.articleForm.get('videos') as FormArray; }
 
+  /**
+   * Sets the article values to the form controls.
+   */
   rebuildForm() {
     console.log('@ArticleNewComponent.rebuildForm()');
     this.articleForm.reset({
+      _id:                this.article._id || null,
+      category:           this.getArticleCategory(this.article.category._id),
       name:               this.article.name || '',
       description:        this.article.description || '',
       handover:           this.article.handover || '',
+
       pictureOverview:    this.article.pictureOverview || '',
       tags:               this.article.tags || '',
+      status:             this.getArticleStatus(this.article.status._id),
+      donee:              {
+        _id:              this.article.donee ? this.article.donee._id : null,
+        firstname:        this.article.donee ? this.article.donee.firstname : '',
+        lastname:         this.article.donee ? this.article.donee.lastname : ''
+      },
       donationDate:       this.article.donationDate || null,
 
       publisher:          {
         _id:              this.article.publisher ? this.article.publisher._id : null,
         firstname:        this.article.publisher ? this.article.publisher.firstname : '',
-        lastname:         this.article.publisher ? this.article.publisher.lastname : '' },
-
-      donee:              {
-        _id:              this.article.donee ? this.article.donee._id : null,
-        firstname:        this.article.donee ? this.article.donee.firstname : '',
-        lastname:         this.article.donee ? this.article.donee.lastname : '' },
-
-      category:           this.article.category || articleCategories[0],
-      status:             this.article.status || articleStatus[0],
-
+        lastname:         this.article.publisher ? this.article.publisher.lastname : ''
+      },
       createdAt:          this.article.createdAt || null,
       updatedAt:          this.article.updatedAt || null,
-      _id:                this.article._id || null
     });
 
     this.setPictures(this.article.pictures);
     this.setVideos(this.article.videos);
   }
 
+  getArticleCategory(_id: string): ArticleCategory {
+    if (!_id) { return  articleCategories[0] ; } // default value
+    return this.articleCategories.find(category => category._id === _id);
+  }
+
+  getArticleStatus(_id: string): ArticleStatus {
+    if (!_id) { return  articleStatus[0] ; } // default value
+    return this.articleStatus.find(status => status._id === _id);
+  }
+
+  /**
+   * Sets the formControl values to a new article object.
+   * @returns {Article}
+   */
   prepareSaveArticle(): Article {
     console.log('@ArticleNewComponent.prepareSaveArticle()');
     const formModel = this.articleForm.value;
 
-    // Create copies of the pictures array items to prevent changing the original values of this.article by entry form.
+    // Create copies of the pictures / videos array items to prevent changing the original values of this.article by entry form.
     const picturesArray: string[] = formModel.pictures.map(
       (picture) => (picture as string)
     );
+    const videosArray: string[] = formModel.videos.map(
+      (video) => (video as string)
+    );
 
     const saveArticle = new Article();
-    saveArticle._id =             this.article._id;
+    saveArticle._id =             this.article._id || null;
+    saveArticle.category =        (formModel.category as ArticleCategory);
     saveArticle.name =            formModel.name as string;
     saveArticle.description =     formModel.description as string;
     saveArticle.handover =        formModel.handover as string;
+
     saveArticle.pictureOverview = formModel.pictureOverview as string;
     saveArticle.pictures =        picturesArray;
+    saveArticle.videos =          videosArray;
     saveArticle.tags =            formModel.tags as string;
+    saveArticle.status =          (formModel.status as ArticleStatus);
+
+    saveArticle.donee =           this.article.donee,
     saveArticle.donationDate =    formModel.donationDate as Date;
 
     saveArticle.publisher =       this.article.publisher;
-    saveArticle.donee =           this.article.donee,
-    saveArticle.category =        (formModel.category as ArticleCategory);
-    saveArticle.status =          (formModel.status as ArticleStatus);
-
-    saveArticle.createdAt =        this.article.createdAt || null,
-    saveArticle.updatedAt =        null,
-    saveArticle._id =              this.article._id || null;
+    saveArticle.createdAt =       this.article.createdAt || null,
+    saveArticle.updatedAt =       null;
 
     return saveArticle;
   }
@@ -192,20 +218,20 @@ export class ArticleNewComponent implements OnInit, OnChanges {
   onSubmit() {
     console.log('@ArticleNewComponent.onSubmit()');
 
-    this.article = this.prepareSaveArticle();
+    let articleToSave = this.prepareSaveArticle();
 
     if (this.editMode === EditModeEnum.CREATE) {
-      this.articleService.createArticle(this.article).subscribe(
-        (article: Article) => {
-          this.article = article;
+      this.articleService.createArticle(articleToSave).subscribe(
+        (savedArticle: Article) => {
+          this.article = savedArticle;
           this.editMode = EditModeEnum.UPDATE;
           this.rebuildForm();
         }
       );
     } else if (this.editMode === EditModeEnum.UPDATE) {
-      this.articleService.updateArticle(this.article).subscribe(
-        (article: Article) => {
-          this.article = article;
+      this.articleService.updateArticle(articleToSave).subscribe(
+        (savedArticle: Article) => {
+          this.article = savedArticle;
           this.rebuildForm();
         }
       );
